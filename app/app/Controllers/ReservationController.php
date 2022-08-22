b276f9956d9783f9aa780b886c994c7adea3fa2f
raresonescu <raresonescu@gmail.com>
2022-08-22 09:04:46 +0300

    change the button

--- app/app/Controllers/ReservationController.php
+++ app/app/Controllers/ReservationController.php
@@ -5,11 +5,9 @@ namespace App\Controllers;
 
 use App\Auth\Auth;
 use App\Entities\Location;
-use App\Entities\User;
 use App\Entities\Appointment;
 use App\Views\View;
 use Doctrine\ORM\EntityManager;
-use JetBrains\PhpStorm\NoReturn;
 use Laminas\Diactoros\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -24,13 +22,11 @@ class ReservationController extends Controller
     public function index(ServerRequestInterface $request): ResponseInterface
     {
         $locations = $this->db->getRepository(Location::class)->findAll();
-
+        //dd($this->db->getRepository(Appointment::class)->findAll());
         return $this->view->render(new Response, 'templates/reservation.twig',['locations'=> $locations]);
+
     }
 //----Create appointment----
-
-
-
 
      public function store(ServerRequestInterface $request): ResponseInterface
 
@@ -39,24 +35,22 @@ class ReservationController extends Controller
 
         $this->createAppointment($data);
 
-        //$this->auth->attempt($data['email'], $data['password']);
-
         return $this->view->render(new Response, 'templates/calendar.twig');
     }
-
 
     protected function createAppointment(array $data): Appointment
     {
 
         $appointment = new Appointment();
-        $locations = $this->db->getRepository(Location::class)->findAll();
-        $x = \DateTime::createFromFormat('Y-m-d', $data['date']);
+        $location= $this->db->getRepository(Location::class)->find($data['location']);
+        $reservation = \DateTime::createFromFormat('Y-m-d', $data['date']);
 
         $appointment->fill([
-            'reservation' => $x,
-            'location' => $locations[$data['location']-1],
+            'reservation' => $reservation,
+            'location' => $location,
             'user' => $this->auth->user(),
         ]);
+
 
         $this->db->persist($appointment);
         $this->db->flush();
@@ -68,7 +62,7 @@ class ReservationController extends Controller
         return $this->validate($request, [
             'date' => ['required'],
             'location' => ['required'],
-            //'user' => ['required']
+
 
         ]);
 
