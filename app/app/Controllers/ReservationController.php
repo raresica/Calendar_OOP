353efdc352ead73d2e6c0a0345efecdededc207f
raresonescu <raresonescu@gmail.com>
2022-08-25 12:57:01 +0300

    can't make appointment on previous dates

--- app/app/Controllers/ReservationController.php
+++ app/app/Controllers/ReservationController.php
@@ -16,7 +16,7 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class ReservationController extends Controller
 {
-    public function __construct(protected View $view,protected Router $router, protected Flash $flash, protected EntityManager $db, protected Auth $auth)
+    public function __construct(protected View $view, protected Router $router, protected Flash $flash, protected EntityManager $db, protected Auth $auth)
     {
 
     }
@@ -29,27 +29,29 @@ class ReservationController extends Controller
 
 //        $urldate= substr($_SERVER['HTTP_REFERER'],36);
 //        dd($request->getQueryParams());
-        $urldate= $request->getQueryParams()["date"];
+        $urldate = $request->getQueryParams()["date"];
         $urldate = \DateTime::createFromFormat('Y-m-d', $urldate);
 
         $urldate = $urldate->format('Y-m-d');
-        return $this->view->render(new Response, 'templates/reservation.twig',['locations'=> $locations, 'urldate'=> $urldate]);
+        return $this->view->render(new Response, 'templates/reservation.twig', ['locations' => $locations, 'urldate' => $urldate]);
 
     }
+
 //----Create appointment----$request->getQueryParams()
 
-     public function store(ServerRequestInterface $request): ResponseInterface
+    public function store(ServerRequestInterface $request): ResponseInterface
 
     {
+//        dd($request);
         $data = $this->validateAppointment($request);
-        if ($this->validateTodayReservation($data))
-        {
+        if ($this->validateTodayReservation($data) && $this->validatePreviousReservation($request->getParsedBody()['date'])) {
             $this->createAppointment($data);
             return redirect($this->router->getNamedRoute('calendar')->getPath());
+        } else {
+            $this->flash->now('error', 'You can not make an appointment on the same day');
         }
 
-         $this->flash->now('error', 'You can not make an appointment on the same day');
-         return redirect($this->router->getNamedRoute('calendar')->getPath());
+        return redirect($this->router->getNamedRoute('calendar')->getPath());
 
 
     }
@@ -60,12 +62,49 @@ class ReservationController extends Controller
 //        $this->createAppointment($data);
 //         return redirect($this->router->getNamedRoute('calendar')->getPath());
 
+    private function validateAppointment(ServerRequestInterface $request): array
+
+    {
+
+        return $this->validate($request, [
+            'date' => ['required'],
+            'location' => ['required'],
+
+        ]);
+
+    }
+
+    private function validateTodayReservation(array $data): bool
+    {
+        $reservationDate = $this->db->getRepository(Appointment::class)->count([
+            'reservation' => \DateTime::createFromFormat('Y-m-d', $data['date']),
+            'user' => $this->auth->user()
+        ]);
+        if (($reservationDate != 0))
+            return false;
+        else {
+            return true;
+        }
+    }
+
+    private function validatePreviousReservation(string $date): bool
+    {
+        $selectedDate = date('Y-m-d', strtotime($date));
+        $todayDate = date('Y-m-d');
+
+
+        if ($selectedDate < $todayDate)
+            return false;
+        else {
+            return true;
+        }
+    }
 
     protected function createAppointment(array $data): Appointment
     {
 
         $appointment = new Appointment();
-        $location= $this->db->getRepository(Location::class)->find($data['location']);
+        $location = $this->db->getRepository(Location::class)->find($data['location']);
         $reservation = \DateTime::createFromFormat('Y-m-d', $data['date']);
 
 
@@ -80,33 +119,6 @@ class ReservationController extends Controller
         $this->db->flush();
         return $appointment;
     }
-    private function validateAppointment(ServerRequestInterface $request): array
-
-    {
-
-        return $this->validate($request, [
-            'date' => ['required'],
-            'location' => ['required'],
-
-        ]);
-
-    }
-
-        private function validateTodayReservation(array $data): bool
-    {
-        $reservationToday = $this->db->getRepository(Appointment::class)->count([
-            'reservation' => \DateTime::createFromFormat('Y-m-d', $data['date']),
-            'user' => $this->auth->user()
-        ]);
-        if (($reservationToday != 0))
-            return false;
-        else {
-            return true;
-        }
-    }
-
-
-
 
 
 }
